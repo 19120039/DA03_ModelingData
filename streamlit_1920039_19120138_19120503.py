3fe3c4fd6d8703adb7687bbebe52e7f1a09b9115
19120039 <57978985+19120039@users.noreply.github.com>
2021-12-31 19:08:22 +0700

    Update streamlit_1920039_19120138_19120503.py

--- streamlit_1920039_19120138_19120503.py
+++ streamlit_1920039_19120138_19120503.py
@@ -93,16 +93,13 @@ form = st.form(key="annotation")
 
 with form:
     #cols = st.columns((10, 10))
-    model_type = st.selectbox(
-        "Choose Model:", ["Decicison Tree", "Logistic Regression"], index=1
-    )
-    while(1):
-        if (model_type == "Decicison Tree"):
-            image = Image.open('decisiontree.jpg')
-            st.image(image)
-        else:
-            image = Image.open('fake-news-3.png')
-            st.image(image)
+    model_type = st.selectbox("Choose Model:", ["Decicison Tree", "Logistic Regression"], index = 1)
+    if (model_type == "Decicison Tree"):
+       image = Image.open('decisiontree.jpg')
+       st.image(image)
+    else:
+       image = Image.open('fake-news-3.png')
+       st.image(image)
     user_input = st.text_input("Write your incredible News here:")
     cols = st.columns(2)
     submitted = st.form_submit_button(label = "Enter your News")
